9c549d0ba8c9c5ddc6fe86a855ee9b3ce1a22417
amarshraddhachinchane <amarshraddhachinchane@gmail.com>
2025-08-07 13:04:55 +0530

    New commit

--- obj/Debug/net8.0/Authentication.AssemblyInfo.cs
+++ obj/Debug/net8.0/Authentication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9083b936ad2feb329712f87075b3bf7111b64f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a94227d4bc1119be8c683793b7d52db71bd22d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
